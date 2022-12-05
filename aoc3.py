223998b360b8c7de155f60b5c64cc853cdcacda3
Anton Sedin <anton.sedin@gmail.com>
2022-12-05 07:33:45 +0100

    day3

---
+++ aoc3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env/ python3
+
+priosum = 0
+badgepriosum = 0
+s = set()
+with open("inputs/input3.txt") as f:
+    for i, line in enumerate(f):
+        line = line.strip()
+        print(line)
+        mid = len(line)//2
+        c = set(line[:mid]).intersection(line[mid:]).pop()
+        if c.islower():
+            prio = ord(c) - 96
+            priosum += prio
+        else:
+            prio = ord(c) - 38
+            priosum += prio
+        if i % 3 == 0:
+            s = set(line)
+        elif i % 3 == 1:
+            s = s.intersection(line)
+        else:
+            print(s.intersection(line))
+            c = s.intersection(line).pop()
+            print(c)
+            if c.islower():
+                prio = ord(c) - 96
+                badgepriosum += prio
+            else:
+                prio = ord(c) - 38
+                badgepriosum += prio
+
+print(priosum)
+print(badgepriosum)
