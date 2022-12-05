74320a2a07fe913c3e2938cb475c983483c98ddc
Anton Sedin <anton.sedin@gmail.com>
2022-12-05 07:52:05 +0100

    day1: add part 1

--- aoc1.py
+++ aoc1.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env/ python3
 
 curr = 0
-maxi = [0, 0, 0]
+maxi = 0
+maxis = [0, 0, 0]
 
 with open("inputs/input1.txt") as f:
     for line in f:
         if(line == '\n'):
-            if curr > min(maxi):
-                maxi[maxi.index(min(maxi))] = curr
+            if curr > maxi:
+                maxi = curr
+            if curr > min(maxis):
+                maxis[maxis.index(min(maxis))] = curr
             curr = 0
         else:
             curr += int(line)
 
-print(sum(maxi))
+print(maxi)
+print(sum(maxis))
