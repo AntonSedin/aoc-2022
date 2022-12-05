454fbc23f5e92741c7022274427d0c00f4cea87b
Anton Sedin <anton.sedin@gmail.com>
2022-12-05 07:35:08 +0100

    day5

---
+++ aoc5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env/ pytho
+
+setup = True
+stacks = [[] for x in range(9)]
+
+with open("inputs/input5.txt") as f:
+    for line in f:
+        if setup:
+            if line == '\n':
+                setup = False
+                for stack in stacks:
+                    stack = stack.reverse()
+                continue
+            for i, j in enumerate(range(1, 35, 4)):
+                c = line[j]
+                if c.isdigit() or c == ' ':
+                    continue
+                stacks[i].append(line[j])
+        else:
+            line = line.strip()
+            [num, fro, to] = [int(x) for x in line.split() if x.isdigit()]
+            # 1
+            #for _ in range(num):
+            #    stacks[to-1].append(stacks[fro-1].pop())
+            for box in stacks[fro-1][-num:]:
+                stacks[to-1].append(box)
+            stacks[fro-1] = stacks[fro-1][:-num]
+
+for stack in stacks:
+    print(stack.pop())
