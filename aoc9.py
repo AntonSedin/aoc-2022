cc2efd9b54db062bddde015bf68842a5633100b7
Anton Sedin <anton.sedin@gmail.com>
2022-12-10 17:39:47 +0100

    day9: part 2

--- aoc9.py
+++ aoc9.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env/ python3
 
-head = [0, 0]
-tail = [0, 0]
-visited = set()
+knots = [[0,0] for i in range(10)]
+unit = {'U': (0,1), 'D': (0,-1), 'L': (-1,0), 'R': (1,0)}
+visited1 = set()
+visited2 = set()
 
 with open("inputs/input9.txt") as f:
     for line in f.readlines():
         direc, steps = line.strip().split()
         for _ in range(int(steps)):
-            if direc == 'U':
-                head[1] += 1
-                if abs(head[1] - tail[1]) >= 2:
-                    tail[1] += 1
-                    tail[0] = head[0]
-            elif direc == 'D':
-                head[1] -= 1
-                if abs(head[1] - tail[1]) >= 2:
-                    tail[1] -= 1
-                    tail[0] = head[0]
-            elif direc == 'L':
-                head[0] -= 1
-                if abs(head[0] - tail[0]) >= 2:
-                    tail[0] -= 1
-                    tail[1] = head[1]
-            elif direc == 'R':
-                head[0] += 1
-                if abs(head[0] - tail[0]) >= 2:
-                    tail[0] += 1
-                    tail[1] = head[1]
-            visited.add(tuple(tail))
+            step = unit[direc]
+            knots[0][0] += unit[direc][0]
+            knots[0][1] += unit[direc][1]
+            for i in range(1, len(knots)):
+                if (abs(knots[i-1][0] - knots[i][0]) == 2
+                and abs(knots[i-1][1] - knots[i][1]) == 2):
+                    knots[i][0] += (knots[i-1][0] - knots[i][0]) // 2
+                    knots[i][1] += (knots[i-1][1] - knots[i][1]) // 2
+                elif knots[i-1][0] - knots[i][0] > 1:
+                    knots[i][0] += 1
+                    knots[i][1] = knots[i-1][1]
+                elif knots[i-1][0] - knots[i][0] < -1:
+                    knots[i][0] -= 1
+                    knots[i][1] = knots[i-1][1]
+                elif knots[i-1][1] - knots[i][1] > 1:
+                    knots[i][1] += 1
+                    knots[i][0] = knots[i-1][0]
+                elif knots[i-1][1] - knots[i][1] < -1:
+                    knots[i][1] -= 1
+                    knots[i][0] = knots[i-1][0]
+                else:
+                    break
 
-print(len(visited))
+            visited1.add(tuple(knots[1]))
+            visited2.add(tuple(knots[9]))
+
+print(len(visited1))
+print(len(visited2))
